a081763cf052dc4e6dfd123e3f6b0407364914ef
mepurion <dawid123793@gmail.com>
2020-12-01 11:27:28 +0100

    Some fixes

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ destination = float(input("Enter the destination between two points: [mm] "))
 if second > first:
     second, first = first, second
 distinction = int((first - second)*100)/100
-print("%s m = %s mm" % (distinction, destination))
+print(f"{distinction} m = {destination} mm")
 supplement = (int((int(first) - second)*100))/100
 point = (int((destination * supplement) / distinction*1000))/1000
-print("%s m = %s mm" % (supplement, point))
+print(f"{supplement} m = {point} mm")
+print("So far, it does not calculates more than one point!")
